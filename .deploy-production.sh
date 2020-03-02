ee21e13886f10a486dfd604d9f674f23d4c5a909
Nic Schumann <nicolas_schumann@brown.edu>
2020-03-01 23:25:33 -0500

    [deploy] updates deploy scripts for msu

--- .deploy-production.sh
+++ .deploy-production.sh
@@ -8,23 +8,20 @@ source ./.env
 #scp -P $KINSTA_PORT -r wp-content/uploads $KINSTA_USER@$KINSTA_IP:./public/wp-content/
 
 # Custom Theme
-scp -P $KINSTA_PRODUCTION_PORT -r wp-content/themes/custom $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/themes
+# scp -r wp-content/themes/custom $MSU_PRODUCTION_USER@$MSU_PRODUCTION_IP:./public_html/feedbackloop/wp-content/themes
 
 # Bundles only
-#scp -P $KINSTA_PRODUCTION_PORT -r wp-content/themes/custom/bundles $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/themes/custom
+scp -r wp-content/themes/custom/bundles $MSU_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public_html/feedbackloop/wp-content/themes/custom
 
 # Plugins and must use plugins
-#scp -P $KINSTA_PRODUCTION_PORT -r wp-content/plugins $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/
-#scp -P $KINSTA_PRODUCTION_PORT -r wp-content/mu-plugins $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/
+# scp -r wp-content/plugins $MSU_PRODUCTION_USER@$MSU_PRODUCTION_IP:./public_html/feedbackloop/wp-content/
+# scp -r wp-content/mu-plugins $MSU_PRODUCTION_USER@$MSU_PRODUCTION_IP:./public_html/feedbackloop/wp-content/
 
 #specific plugins
-#scp -P $KINSTA_PRODUCTION_PORT -r wp-content/plugins/wc-product-customer-list-premium $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/plugins
+# scp -r wp-content/plugins/classic-editor $MSU_PRODUCTION_USER@$MSU_PRODUCTION_IP:./public_html/feedbackloop/wp-content/plugins
 
 #specific files
 #scp -P $KINSTA_PRODUCTION_PORT wp-content/themes/custom/functions/post-types/classes/class-nam-class.php $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/themes/custom/functions/post-types/classes/
 
 #functions.php
 #scp -P $KINSTA_PRODUCTION_PORT wp-content/themes/custom/functions.php $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/themes/custom/
-
-
-curl -L $PRODUCTION_SITE_URL/kinsta-clear-cache-all/
